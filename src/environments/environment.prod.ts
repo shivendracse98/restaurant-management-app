eb1fef35e4fb03b06f7337b8b325becc6614094d
Shivendra Yadav <shivendra.cse.98@gmail.com>
2025-12-31 19:06:28 +0530

    prepare frontend for cloudflare deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://localhost:8080/api' // Set to localhost for local testing/PWA verification
+    apiBaseUrl: 'https://restaurant-backend-app-tfye.onrender.com/api'
 };
